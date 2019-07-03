73e8577da4f4f8220e2b66d24cd5a6385f8fa5a0
SG24 <shreyanshgupta98@gmail.com>
2019-07-03 14:41:09 +0530

    adds organisation schema

---
+++ server/models/Organisation.js
@@ -0,0 +1,42 @@
+// Declaring and Exporting organisation model
+
+// requiring modules
+let mongoose = require("mongoose");
+
+// defining schema
+let orgSchema = new mongoose.Schema({
+
+  // name of the organisation
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // description for the organisation
+  description: {
+    type: String,
+    required: true,
+  },
+  // the user who created the organisation
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  // TODO: do we include the creator in the list of members or just keep that user separate
+  // array of users in the organisation
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+  // optional image of the organisation to be displayed on its page
+  organisationImage: {
+    type: String,
+  },
+
+}, {
+    timestamps: true
+  });
+
+// requiring and exporting organisation model
+let Organisation = mongoose.model("Organisation", orgSchema);
+module.exports = Organisation;
